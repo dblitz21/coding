61f40363ef70e4ae173016356142b13b9032dda2
dblitz <blitzy2k@gmail.com>
2019-06-14 13:58:43 -0400

    Listings

---
+++ bicycles.py
@@ -0,0 +1,16 @@
+bicycles = ["trek", "cannondale", "redline", "specialized"]
+
+print(bicycles)
+print(bicycles[0])
+
+#First letter capitalized
+print(bicycles[1].title())
+
+#Last item in a list
+print(bicycles[-1])
+
+print("\n")
+
+message = "My first bicycle was a " + bicycles[2].title() + "."
+
+print(message)
