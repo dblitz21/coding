6eefb83519fef191e15b60c85f30d729236456ee
dblitz <blitzy2k@gmail.com>
2019-06-16 23:42:14 -0400

    Clue List

---
+++ guest_list.py
@@ -0,0 +1,8 @@
+guest_list = ["Colonel Mustard", "Professor Plum", "Mr. Green"]
+print(guest_list)
+
+guest_list.append("Ms. Scarlet")
+guest_list.insert(1, "Mrs White")
+guest_list.insert(4, "Mrs Peacock")
+
+print(guest_list) 
