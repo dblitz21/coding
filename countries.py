ff793def7db5f68fd652ef6ba3042052d6f9cd16
dblitz <blitzy2k@gmail.com>
2019-06-18 07:43:09 -0400

    Listing improved

---
+++ countries.py
@@ -0,0 +1,12 @@
+countries = ["Spain", "China", "England", "Thailand", "India"]
+print(countries)
+print(sorted(countries))
+print(countries)
+countries.reverse()
+print(countries)
+countries.reverse()
+print(countries)
+countries.sort()
+print(countries)
+countries.reverse()
+print(countries)
--- guest_list.py
+++ guest_list.py
@@ -4,8 +4,20 @@ print(guest_list)
 guest_list.append("Ms. Scarlet")
 guest_list.insert(1, "Mrs White")
 guest_list.insert(4, "Mrs Peacock")
+guest_list.append("Mr. Boddy")
 
 print(guest_list)
+numberofguests = len(guest_list)
+print("\nThere are " + str(numberofguests) + " guests at the party.")
+
+victim = guest_list.pop()
+print("\n " + victim + " has left the party." + "\n")
+numberofguests = len(guest_list)
+
+print("There are now " + str(numberofguests) + " guests at the party.\n")
+print(guest_list) 
+
+
 
 suspect = guest_list[-1]
 
