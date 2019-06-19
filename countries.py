ddbfdcc04d333d69320abe777f5ad171b47f62b6
dblitz <blitzy2k@gmail.com>
2019-06-19 11:13:27 -0400

    sorting and looping

--- countries.py
+++ countries.py
@@ -10,3 +10,6 @@ countries.sort()
 print(countries)
 countries.reverse()
 print(countries)
+
+print("\nThere are " + str(len(countries)) + " countries in this list")
+print("\nThe first country in this list is " + countries[0] + ".")
---
+++ pizzas.py
@@ -0,0 +1,6 @@
+pizzas = ["plain", "pepperoni", "mushroom", "anchovy", "jalapeno", "extra cheese"]
+
+for pizza in pizzas:
+	print("I like " + pizza.title() + " pizza! \n") 
+
+print("I really love Pizza!")
