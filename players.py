dd11c657253d7fd5d9604ed5403d445065cb674d
dblitz <blitzy2k@gmail.com>
2019-06-26 23:53:58 -0400

    More work with slicing

--- players.py
+++ players.py
@@ -2,3 +2,7 @@ players = ['charles', 'martina', 'michael', 'florence', 'eli']
 
 #print a slice of a list
 print(players[0:3])
+
+print("\nHere are the first 3 players on my team:")
+for player in players[:3]:
+	print(player.title())
