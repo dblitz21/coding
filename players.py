81ef01033e1d03284c436343421ed5c85c247518
dblitz <blitzy2k@gmail.com>
2019-06-27 23:16:34 -0400

    More work with Slices

--- players.py
+++ players.py
@@ -1,7 +1,17 @@
 players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players)
 
 #print a slice of a list
 print(players[0:3])
+
+#print up to index 4
+print(players[:4])
+
+#print after index 2
+print(players[2:])
+
+#print the last 3 items in a list no mater what if the size changes
+print(players[-3:])
 
 print("\nHere are the first 3 players on my team:")
 for player in players[:3]:
