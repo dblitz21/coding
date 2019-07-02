1eccadd4f1ead822542f994c9444d8f677c23c9a
dblitz <blitzy2k@gmail.com>
2019-07-01 23:36:49 -0400

    Pep8 and conditionals 1

--- buffet.py
+++ buffet.py
@@ -4,7 +4,9 @@ print("The foods this buffet offers are:")
 for item in buffet_menu:
 	print(item.title())
 
-buffet_menu = ('mussels', 'beef with broccoli', 'pizza', 'sushi', 'shrimp', 'soup')
+#Pep8 standard spacing rules codes end at 79. Comments end at 72 spaces
+buffet_menu = ('mussels', 'beef with broccoli', 'pizza', 'sushi', 'shrimp', 
+'soup')
 print("\nThe modified menu for this buffet is:")
 for item in buffet_menu:
 	print(item.title())
---
+++ toppings.py
@@ -0,0 +1,13 @@
+requested_topping = 'mushroom'
+
+if requested_topping != 'anchovies':
+	print("Hold the anchovies!")
+
+#Check whether a value is in a list
+
+requested_toppings = ['mushrooms', 'onions', 'pineapple']
+
+if 'mushrooms' in requested_toppings:
+	print("true")
+if 'pepperoni' in requested_toppings:
+	print("true")
