5f1822171c28d48ca7f2c60299e66491a7172e85
dblitz <blitzy2k@gmail.com>
2019-07-02 23:49:40 -0400

    More work with conditionals

--- cars2.py
+++ cars2.py
@@ -1,7 +1,14 @@
-cars = ['audi', 'bmw', 'suburu', 'toyota']
+cars = ['audi', 'bmw', 'subaru', 'toyota']
 
 for car in cars:
 	if car == 'bmw':
 		print(car.upper())
 	else:
 		print(car.title())
+
+car = 'subaru'
+print("\nIs car == to subaru? I predict True.")
+print(car == 'subaru')
+
+print("\nIs car == 'audi'? I predict False.")
+print(car=='audi')
