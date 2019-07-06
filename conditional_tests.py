afb6cc00fac00ebe2e2784ef4f5b5dab3f6644a3
dblitz <blitzy2k@gmail.com>
2019-07-05 23:44:50 -0400

    Additional conditionals

---
+++ conditional_tests.py
@@ -0,0 +1,14 @@
+color = "green"
+color2 = "Green"
+
+print(color == color2);
+print(color == color2.lower())
+
+print(1 <= 8)
+print(5 <= 2)
+print(5<8 and 2<3)
+
+colors = ['green', 'purple', 'red', 'yellow']
+
+print('blue' in colors)
+print('blue' not in colors)
